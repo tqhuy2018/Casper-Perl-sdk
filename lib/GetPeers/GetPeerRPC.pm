6f96131c5c993917b5da983143aa9d9dc81bf8ee
hienbui9999 <buiduyhien@gmail.com>
2022-05-13 15:53:05 +0700

    Adjust some function calls

--- lib/GetPeers/GetPeerRPC.pm
+++ lib/GetPeers/GetPeerRPC.pm
@@ -7,16 +7,32 @@ use GetPeers::GetPeersResult;
 
 sub new {
 	my $class = shift;
-	my $self = {};
+	my $self = {_url=>shift};
 	bless $self, $class;
 	return $self;
 }
+
+# get-set method for _url
+sub setUrl {
+	my ($self,$value) = @_;
+	$self->{_url} = $value if defined ($value);
+	return $self->{_url};
+}
+sub getUrl {
+	my ($self) = @_;
+	return $self->{_url};
+}
+
 =comment
 This function does info_get_peers RPC call
 =cut
 sub getPeers {
 	 my( $self ) = @_;
-	my $uri = 'https://node-clarity-testnet.make.services/rpc';
+	my $uri = $self->{_url};
+	if($uri) {
+	} else {
+		$uri = $Common::ConstValues::TEST_NET;
+	}
 	my $json = '{"params" :  [], "id" :  1, "method": "info_get_peers", "jsonrpc" :  "2.0"}';
 	my $req = HTTP::Request->new( 'POST', $uri );
 	$req->header( 'Content-Type' => 'application/json');
--- lib/GetStateRootHash/GetStateRootHashRPC.pm
+++ lib/GetStateRootHash/GetStateRootHashRPC.pm
@@ -5,6 +5,8 @@ use LWP::UserAgent;
 use Data::Dumper;
 use JSON qw( decode_json );
 
+our $VERSION = 0.01;
+
 sub new {
 	my $class = shift;
 	my $self = {_url=>shift};
@@ -22,8 +24,18 @@ sub getUrl {
 	my ($self) = @_;
 	return $self->{_url};
 }
-
-# This function does chain_get_state_root_hash RPC call
+=comment
+This function does chain_get_state_root_hash RPC call
+The procedure for this process is:
+1. Assign the url for the post method, by default the url will be the test net 
+(url for test net is: https://node-clarity-testnet.make.services/rpc which defined in Common::ConstValues class)
+You can change the url to main net or localhost for the method by calling the function setUrl.
+2. Get the Post parameter to send along with the Post method. In this method it is defined in this $json variable, at this code line:
+my $json = $list[1];
+3. Send the post method and read the response data back.
+If the response error encapsulated in json, there will be error thrown.
+Otherwise, the state root hash is retrieved by parsing the json data back, using $decoded->{'result'}{'state_root_hash'}
+=cut
 sub getStateRootHash {
 	my @list = @_;
 	my $uri = $self->{_url};
