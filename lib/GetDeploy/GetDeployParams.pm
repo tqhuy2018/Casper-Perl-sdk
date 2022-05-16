afed0052ee2dad0964ab637964face6ab56cea44
hienbui9999 <buiduyhien@gmail.com>
2022-05-16 15:42:44 +0700

    Update GetDeployParams.pm

--- lib/GetDeploy/GetDeployParams.pm
+++ lib/GetDeploy/GetDeployParams.pm
@@ -2,6 +2,13 @@
 This class generate the parameter for sending for POST method of info_get_deploy RPC call
 The generated parameter is somehow like this:
 {"id" :  1, "method" :  "info_get_deploy", "params" :  {"deploy_hash" :  "6e74f836d7b10dd5db7430497e106ddf56e30afee993dd29b85a91c1cd903583"}, "jsonrpc" :  "2.0"}
+The only part in the Json string is different is the "deploy_hash" value, based on the _deployHash value of the GetDeployParams object.
+To generate the parameter, simple instantiate one GetDeployParams object
+my $gdp = new GetDeploy::GetDeployParams();
+Then assign the _deployHash variable value
+$gdp->setDeployHash("one valid deploy hash value here");
+Then you can get the parameter for the info_get_deploy RPC method call by calling this function
+my $parameterStr = $gdp->generateParameterStr();
 =cut
 package GetDeploy::GetDeployParams;
 
